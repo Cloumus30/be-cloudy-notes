808be60d2737cfd581402e697891c674b926abab
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-02-28 21:17:08 +0700

    update cors

--- index.ts
+++ index.ts
@@ -18,13 +18,13 @@ dotenv.config();
 const app:Express = express();
 const port = process.env.APP_PORT || 3000; 
 
-app.use(cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  }));
-app.options('*', cors())
+// app.use(cors({
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204
+//   }));
+// app.options('*', cors())
 
 
 app.use(express.json());
