fb0b1ad22775662d5ac89de21676ef5ddebad048
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-02-28 20:50:06 +0700

    Add preflight cors

--- index.ts
+++ index.ts
@@ -18,8 +18,14 @@ dotenv.config();
 const app:Express = express();
 const port = process.env.APP_PORT || 3000; 
 
-app.use(cors({origin:true}));
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }));
 app.options('*', cors())
+
 app.use(express.json());
 
 app.use(pagination);
