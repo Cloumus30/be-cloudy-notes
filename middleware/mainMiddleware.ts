6281f30bbb5d629b4b040c7bcd855132c33246dd
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-06-04 20:18:59 +0700

    Update pagination limit to 100

--- middleware/mainMiddleware.ts
+++ middleware/mainMiddleware.ts
@@ -3,7 +3,7 @@ import { createPaginator } from "prisma-pagination";
 
 export const pagination = (req: Request, res: Response, next: NextFunction) =>{
     const page:any = req.query.page || 1;
-    const perPage:any = req.query?.perPage || 10;
+    const perPage:any = req.query?.perPage || 100;
     req.body.paginate = createPaginator({page, perPage});
 
     next();
